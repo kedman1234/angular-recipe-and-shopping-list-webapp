a7ecb17994ae1895708ffe2aecf9da933c77fc98
kedar vijay kulkarni <kedman1234@gmail.com>
2019-09-08 01:47:40 +0530

    add recipe item object to the html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-	recipes = [];
-	
+	recipes: Recipe[] = [
+		new Recipe('Peppy Paneer Pizza', 'This is simply a test', 'https://www.dominos.co.in//files/items/Paneer_Special.jpg')
+	];
+
   constructor() { }
 
   ngOnInit() {
